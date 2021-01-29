6ed36e84153a40664c13ad90cc9962e275517c56
Your Name <assadtahir93@gmail.com>
2021-01-29 12:49:22 +0100

    etape 1

---
+++ animation.js
@@ -0,0 +1,25 @@
+
+// fonction afficher reduire
+function displayDiv(id) {
+  var div = document.getElementById(id);
+  if (div.style.display === "none") {
+    div.style.display = "block";
+    
+  } else {
+    div.style.display = "none";
+  }
+  
+}
+
+
+// Fonction random
+function getRandomInt(array) {
+  return Math.floor(Math.random() * array.length) // recupere nombre aleatoire par rapport a la taile de tableau
+}
+// Tableau
+function random() {
+  var listeApprenant = ['Farima','Asad','Abdoulaye','Alexis','Alicia','Ayoub','Bamba','Caroline','Charles','David','Denis','Fodie','Elyesse','Flora','Gaelle','Jules','Kevin','Lobna','Jonathan','Lyes','Maelys','Muhammad','Mohamed','Nasser']; 
+  // tableau des apprenant
+  var alea = getRandomInt(listeApprenant)  //
+  document.getElementById('boutonrandom').value = listeApprenant[alea]
+}
